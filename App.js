425aace00f8cb0bcfcdb4be42b85af07ca397595
adamhinton <adam.d.hinton@gmail.com>
2024-04-28 21:25:20 -0400

    my first react native useRef

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import MyText from './components/MyText/MyText';
+import React, {useRef} from 'react';
+import {SafeAreaView, Text, ScrollView, Button} from 'react-native';
 
 const App = () => {
+  let array = Array(1000).fill(0);
+
+  const scrollViewRef = useRef(null);
+
+  const handleClick = () => {
+    scrollViewRef.current.scrollTo({x: 0, y: 0, animated: true});
+  };
   return (
-    <SafeAreaView>
-      <MyText name="Bob" />
+    <SafeAreaView style={{flex: 1}}>
+      {/* UseRef is used to store a reference to a value; in this case the value is this scrollView component */}
+      <ScrollView ref={scrollViewRef}>
+        {array.map((elem, idx) => (
+          <Text key={idx}>Hello, world! {idx} </Text>
+        ))}
+      </ScrollView>
+      <Button onPress={handleClick} title={'Scroll To Top'} />
     </SafeAreaView>
   );
 };
