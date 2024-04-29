6ada3a82f768dde12ecbee3a991bd9541eb003cc
adamhinton <adam.d.hinton@gmail.com>
2024-04-29 12:18:03 -0400

    setting up for custom hooks lesson

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-/* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
-import {Button, SafeAreaView, Text, View} from 'react-native';
-import {ThemeContext} from './contexts/ThemeContext';
-import HomeScreen from './components/HomeScreen/HomeScreen';
+import React, from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
   return (
-    <ThemeContext.Provider value={isDarkMode}>
-      <SafeAreaView>
-        <View style={{backgroundColor: isDarkMode ? '#222222' : '#ffffff'}}>
-          <Button title={'Switch mode'} onPress={toggleTheme} />
-          <Text>Hello, World!</Text>
-        </View>
-        <HomeScreen />
-      </SafeAreaView>
-    </ThemeContext.Provider>
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
   );
 };
 
